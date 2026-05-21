8c8ea11fe8bc60dd4127990f8cb1d8cfbdf0d5f7
erictita <nkenatita@gmail.com>
2026-05-21 13:01:12 +0200

    index.js modify

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Gitops auto Deploy working fine!\n');
+  res.end('Gitops is good for deployment!\n');
 }).listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
